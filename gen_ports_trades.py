108c3e973f648c82945a5bfb7d0687836292d48e
tollerr <rodrigotoller@hotmail.com>
2013-11-20 18:07:06 -0200

    test

--- gen_ports_trades.py
+++ gen_ports_trades.py
@@ -6,6 +6,7 @@ Created on Nov 19, 2013
 
 import random
 import datetime
+import numpy as np
 
 
 def get_data():
@@ -20,7 +21,7 @@ def get_data():
     #print [[x,random.randrange(200,10000)] for x in ports]
     list_=[(a, b, random.randrange(200,3000)) for a in dateList for b in ports]
     
-    #formated list
+    #formated list, get only weekdays
     flist=[ (a.strftime("%Y/%m/%d"),b,c) for (a,b,c) in list_ if a.isoweekday() in range(1, 6)]
     # for (a,b,c) in list_:
     #     if a.isoweekday() in range(1, 6):
@@ -37,45 +38,67 @@ def test():
             
     for x in xxx:
         print x
+
+
+
+
+def get_indexed_data(data, idx):
+    #create an indexed hash with unique values
+    idx_values = {}
+    dt = set([a[idx] for a in data]) #get unique dates
+    for i, v in enumerate(sorted(dt)): #create hash that returns the index of a date
+        idx_values[v] = i
     
-data=get_data() 
+    return idx_values
 
-#create an indexed hash with uniq dates
-dts={}    
-dt = set([a[0] for a in data]) #get unique dates    
-for i,v in enumerate(sorted(dt)): #create hash that returns the index of a date
-    dts[v]=i
-
-#create an index hash with unique ports
-pts = {}    
-pt = set([a[1] for a in data])
-for i,v in enumerate(sorted(pt)):
-    pts[v]=i
-
-#print dts
-#print sorted(pt)
-#print '\n'.join(sorted(dt))
-
-#for x in sorted(pt):
-#    print "%d-->%d"%(x,pts[x])
-
-#define a matrix
-import numpy as np
-
-#pts = np.array([[2,0],[3,0],[3,1],[5,0],[5,1],[5,2]])
-
-matrix = np.zeros((len(pts),len(dts)), dtype='int32')
-   
-print "*******"*10 
-for x in data:
-    try:
-        matrix[pts[x[1]]][dts[x[0]]] = x[2]
-        #print "(%s,%s)(%d,%d)->%d"%(x[1],x[0],pts[x[1]],dts[x[0]] , x[2])
-    except:
-        print "Failed:",x
+def convert_to_matrix(data):
     
-print matrix
-# for y in matrix:
-#     print y   
+    #get indexed values for the Date - index 0
+    dts = get_indexed_data(data,0)
+
+    #create an index hash with unique ports - index 1
+    pts = get_indexed_data(data,1)    
     
+    ##Note needed
+    #print dts
+    #print sorted(pt)
+    #print '\n'.join(sorted(dt))
     
+    #for x in sorted(pt):
+    #    print "%d-->%d"%(x,pts[x])
+    
+    #define a matrix
+
+    
+
+    matrix = np.zeros((len(pts),len(dts)), dtype='int32')
+       
+  
+    for x in data:
+        try:
+            matrix[pts[x[1]]][dts[x[0]]] = x[2]
+            # print "(%s,%s)(%d,%d)->%d"%(x[1],x[0],pts[x[1]],dts[x[0]] , x[2])
+        except:
+            print "Failed:",x
+     
+    return matrix
+    # for y in matrix:
+    #     print y   
+
+def save_matrix(matrix):
+    np.savetxt("test.csv", matrix, delimiter=",")
+    return 
+ 
+def save_dates(dts):
+    with open('dates.csv','wb') as cfile:
+        cfile.write('\n'.join(dts))
+    return   
+
+
+
+if __name__ == 'main':
+    
+    data=get_data()[:10]
+    print data
+    matrix=convert_to_matrix(data)    
+    print "done"
