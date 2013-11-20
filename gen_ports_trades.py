db635af31784038d4eb85403c918105b1589ccc7
tollerr <rodrigotoller@hotmail.com>
2013-11-20 18:32:10 -0200

    remove comments

--- gen_ports_trades.py
+++ gen_ports_trades.py
@@ -10,30 +10,29 @@ import numpy as np
 
 
 def get_data():
+    #define ports
     ports = [100,101,102,104,110,115,118,400,402,405,410,800,801,802,804,810,815,818]
-    ports = ports[:3]
     
-    
+    #define date interval
     base = datetime.datetime.today()
     numdays=360 * 4
     dateList = [ base - datetime.timedelta(days=x) for x in range(0,numdays) ]
     
-    #print [[x,random.randrange(200,10000)] for x in ports]
+    #create a list with date,port, and a random number
     list_=[(a, b, random.randrange(200,3000)) for a in dateList for b in ports]
     
     #formated list, get only weekdays
     flist=[ (a.strftime("%Y/%m/%d"),b,c) for (a,b,c) in list_ if a.isoweekday() in range(1, 6)]
-    # for (a,b,c) in list_:
-    #     if a.isoweekday() in range(1, 6):
-    #         #print "'%s',%d,%d"%(a.strftime("%Y/%m/%d"),b,c)
-    #         print "'%s',%d,%d"%(a.strftime("%Y/%m/%d"),b,c)
+
+    #return a list of tuple (date,port,rnd)
     return flist
 
 
 
 
     
-def test():    
+def test(): 
+    """ Unused """   
     xxx=get_data()
             
     for x in xxx:
@@ -46,7 +45,7 @@ def get_indexed_data(data, idx):
     """based on an list of list, creates an indexed hash for element at idx
     create an indexed hash with unique values"""
     idx_values = {}
-    dt = sorted(set([a[idx] for a in data])) #get unique items
+    dt = sorted(set([a[idx] for a in data])) #get unique sorted items
     for i, v in enumerate(dt): #create hash that returns the index of a item
         idx_values[v] = i 
     
@@ -57,26 +56,15 @@ def convert_to_matrix(data):
     where value 1 is column and value 2 is row"""
     
     #get indexed values for the Date - index 0
-    dts,dt = get_indexed_data(data,0)
+    dts = get_indexed_data(data,0)
 
     #create an index hash with unique ports - index 1
-    pts,pt = get_indexed_data(data,1)    
-    
-    ##Note needed
-    #print dts
-    #print sorted(pt)
-    #print '\n'.join(sorted(dt))
-    
-    #for x in sorted(pt):
-    #    print "%d-->%d"%(x,pts[x])
-    
-    #define a matrix
-
-    
-
-    matrix = np.zeros((len(pts),len(dts)), dtype='int32')
-       
+    pts = get_indexed_data(data,1)    
   
+    #defines a matrix with zero values
+    matrix = np.zeros((len(pts),len(dts)), dtype='int32')
+     
+    #puts in the matrix the list of (date,port,#) 
     for x in data:
         try:
             matrix[pts[x[1]]][dts[x[0]]] = x[2]
@@ -89,10 +77,12 @@ def convert_to_matrix(data):
     #     print y   
 
 def save_matrix(matrix, filen):
+    """ saves the matrix """
     np.savetxt(filen, matrix, delimiter=",")
     return 
  
 def save_list(lst, filen):
+    """ saves any list """
     with open(filen,'wb') as cfile:
         cfile.write('\n'.join(lst))
     return   
@@ -101,7 +91,8 @@ def save_list(lst, filen):
 
 ###########################3
 # main
-     
+
+    
 data=get_data()[:21]
 
 print '-'*40
